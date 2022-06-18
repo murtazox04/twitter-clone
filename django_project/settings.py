e735125669dbea87787d820b60b1ab03fd2b3340
Developerboys <murtazoxurramov1@gmail.com>
2022-06-18 18:21:36 +0500

    postgres settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'twitter',
+        'USER': 'postgres',
+        'PASSWORD': 'murtazo0012800',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
